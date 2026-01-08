7617082dfc3c58b8d7b9e705a5a85f2ff0905c7f
zinedienelkh-byte <zinedienelkh@gmail.com>
2026-01-08 20:40:57 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-function celebrate() {
-    alert("Success! You are now a member.");
-    console.log("A user clicked the buy button!");
+const button1= document.querySelector('#pro')
+const button2= document.querySelector('#ultimate')
+const button3= document.querySelector('#ma-toggle')
+const pro_price= document.querySelector('#pro-price')
+const ultimate_price= document.querySelector('#ultimate-price')
+let isAnnual= false
+
+button1.addEventListener('click', function() {
+    alert("Success! You are now a Pro Member.")
 }
 
+)
+
+button2.addEventListener('click', function() {
+    alert("Success! You are now an Ultimate Member.")
+}
+
+)
+
+button3.addEventListener('click', function(){
+    if (isAnnual) {
+        pro_price.textContent='$19.99/month'
+        ultimate_price.textContent='$49.99/month'
+        isAnnual= false
+        button3.textContent = 'Switch to Annual';
+    }
+    else {
+        pro_price.textContent='$149.99/year'
+        ultimate_price.textContent='$449.99/year'
+        isAnnual= true
+        button3.textContent = 'Switch to Monthly';
+    }
+}
+
+)
