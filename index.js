4c899cde56346fbbb88ae78d908e73333dc75d42
zinedienelkh-byte <zinedienelkh@gmail.com>
2026-01-09 17:05:43 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const button2= document.querySelector('#ultimate')
 const button3= document.querySelector('#ma-toggle')
 const pro_price= document.querySelector('#pro-price')
 const ultimate_price= document.querySelector('#ultimate-price')
+const dark_mode= document.querySelector('#dark-mode')
+const body= document.querySelector('#body')
 let isAnnual= false
 
 button1.addEventListener('click', function() {
@@ -29,6 +31,17 @@ button3.addEventListener('click', function(){
         ultimate_price.textContent='$449.99/year'
         isAnnual= true
         button3.textContent = 'Switch to Monthly';
+    }
+}
+
+)
+dark_mode.addEventListener('click', function(){
+    body.classList.toggle('dark_mode')
+    if (body.classList.contains('dark_mode')){
+        dark_mode.textContent='☀️ Light Mode'
+    }
+    else {
+        dark_mode.textContent='🌙 Dark Mode'
     }
 }
 
